87a93635c5798ac040891c09ce3c0b38a1a8249d
YoungpilYun <youngfeeli@gmail.com>
2024-01-01 11:40:13 +0900

    detail url change

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -6,7 +6,7 @@ function Movie({ id, coverImg, title, summary, genres }) {
     <div>
       <img src={coverImg} alt={title} />
       <h2>
-        <Link to={`/movie/${id}`}>{title}</Link>
+        <Link to={`/react-2-for-beginners/movie/${id}`}>{title}</Link>
       </h2>
       <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
       <ul>
